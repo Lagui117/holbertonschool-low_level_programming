6e758d2ae84c2b67d83780d1b41d7d15037e7edb
Lagui117 <guillaumewatelet06@gmail.com>
2025-02-18 13:33:06 +0100

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	printf("size int %zu\n", sizeof(int));
+	printf("size char %zu\n", sizeof(char));
+	printf("size double %zu\n", sizeof(double));
+	printf("size float %zu\n", sizeof(float));
+
+	return (0);
+}
