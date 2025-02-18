1689256b295f9625b9e06afdb74ba84b0997814c
Lagui117 <guillaumewatelet06@gmail.com>
2025-02-18 13:58:12 +0100

    hi

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
 */
 int main(void)
 {
-	printf("size char: %zu\n", sizeof(char));
-	printf("size int: %zu\n", sizeof(int));
-	printf("size long: %zu\n", sizeof(long));
-	printf("size long long: %zu\n", sizeof(long long));
-	printf("size float: %zu\n", sizeof(float));
+	printf("Size of a char: %zu\n", sizeof(char));
+	printf("Size of an int: %zu\n", sizeof(int));
+	printf("Size of a long int: %zu\n", sizeof(long int));
+	printf("Size of a long long intSize of a: %zu\n", sizeof(long long int));
+	printf("Size of a float: %zu\n", sizeof(float));
 	return (0);
 }
