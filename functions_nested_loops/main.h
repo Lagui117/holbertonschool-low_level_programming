0df6500272dded8537306450fa243109368ce710
Lagui117 <guillaumewatelet06@gmail.com>
2025-02-23 14:58:08 +0100

    2-print_alphabet_x10.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,17 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 int _putchar(char c);
-
 
 void print_to_98(int n);
 
-
 void print_alphabet(void);
 
+void print_alphabet_x10(void);  
 
 void print_times_table(int n);
 
 #endif 
+
 
