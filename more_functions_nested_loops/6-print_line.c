86ccf49839b475691d915b1b3664fabab9e29736
Lagui117 <guillaumewatelet06@gmail.com>
2025-02-28 22:53:50 +0100

    8-print_square.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- *
- *
- * Return: 
+ * print_line - Prints a line of underscores
+ * @n: Number of underscores to print
  */
-
 void print_line(int n)
 {
-
 	int i;
+
 	if (n > 0)
 	{
-		for (i=0; i < n; i++)
+		for (i = 0; i < n; i++)
 			_putchar('_');
 	}
-
-	_putchar('\n') ;
+	_putchar('\n');
 }
-
