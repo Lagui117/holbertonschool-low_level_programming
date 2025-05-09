a62f2c9cff2420e5ff9de94735b0af0e761dd20b
Lagui117 <guillaumewatelet06@gmail.com>
2025-05-09 14:39:34 +0200

    2-print_alphabe

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times, followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
