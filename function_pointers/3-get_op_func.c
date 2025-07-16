f69894ce6efb7c1c4b40edc9528cb344b58e60fb
Lagui117 <guillaumewatelet06@gmail.com>
2025-07-16 18:07:05 +0200

    pls

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "calc.h"
+
+/**
+ * get_op_func - Sélectionne la fonction correcte à utiliser pour l’opération demandée
+ * @s: L’opérateur sous forme de chaîne (ex: "+", "-", "*", "/", "%")
+ *
+ * Return: Un pointeur vers la fonction correspondant à l’opérateur.
+ *         NULL si l’opérateur ne correspond à aucune opération connue.
+ *
+ * Description:
+ * Cette fonction compare la chaîne `s` aux opérateurs connus
+ * et retourne un pointeur vers la fonction associée à cet opérateur.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (i < 4)
+	{
+		if (!strcmp(ops[i].op, s))
+			return (ops[i].f)
+
+		i++;
+	}
+	return (NULL)
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - Calcule la somme de deux entiers
+ * @a: Premier entier
+ * @b: Deuxième entier
+ *
+ * Return: La somme de a et b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Calcule la différence entre deux entiers
+ * @a: Premier entier
+ * @b: Deuxième entier
+ *
+ * Return: La différence de a et b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Calcule le produit de deux entiers
+ * @a: Premier entier
+ * @b: Deuxième entier
+ *
+ * Return: Le produit de a et b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Calcule la division entière de deux entiers
+ * @a: Numérateur
+ * @b: Dénominateur
+ *
+ * Return: Le quotient de a divisé par b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Calcule le reste de la division entière
+ * @a: Numérateur
+ * @b: Dénominateur
+ *
+ * Return: Le reste de a divisé par b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
