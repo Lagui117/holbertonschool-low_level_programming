a17298ec9c06ef9d9f770a684a31ebfcce4400b9
Lagui117 <guillaumewatelet06@gmail.com>
2025-08-01 12:06:40 +0200

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - libère une liste de type list_t
+ * @head: pointeur vers le début de la liste
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
+
